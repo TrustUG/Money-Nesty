563db9ab0a6030837c812fd270f2cf371eecc83c
Trust Jonathan <trustjonathan205@gmail.com>
2025-07-18 17:43:54 +0300

    5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cardsWallper = document.querySelector(".cards-wallper");
+const chipsWallper = document.querySelector(".chips-wallper");
 
 cardsWallper.addEventListener(
   "wheel",
@@ -6,6 +7,17 @@ cardsWallper.addEventListener(
     if (e.deltaY !== 0) {
       e.preventDefault();
       cardsWallper.scrollLeft += e.deltaY;
+    }
+  },
+  { passive: false }
+);
+
+chipsWallper.addEventListener(
+  "wheel",
+  (e) => {
+    if (e.deltaY !== 0) {
+      e.preventDefault();
+      chipsWallper.scrollLeft += e.deltaY;
     }
   },
   { passive: false }
