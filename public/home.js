6e33ec437e7a2ca7203d8012b39814227c48297a
Trust Jonathan <161218350+TrustUG@users.noreply.github.com>
2025-09-04 20:24:12 +0300

    908

--- public/home.js
+++ public/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".cards-wallper .card").on("click", function () {
     // navigate to the target URL
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
   });
 });
